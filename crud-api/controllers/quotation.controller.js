2103a8a8c6fab717c9cbb27549a6f921164c5f7e
Ivan Andi <56378011+idnanavi@users.noreply.github.com>
2020-10-12 09:33:19 +0700

    Update quotation.controller.js

--- crud-api/controllers/quotation.controller.js
+++ crud-api/controllers/quotation.controller.js
@@ -27,14 +27,13 @@ exports.findById = function(req, res) {
 };
 
 exports.update = function(req, res) {
-  if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-    res.status(400).send({ error:true, message: 'Please provide all required field' });
-  }else{
-    Quotation.update(req.params.id, new Quotation(req.body), function(err, quotation) {
-  
-   res.json({ error:false, message: 'Quotation successfully updated' });
-});
-}
+      if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+          res.status(400).send({ error:true, message: 'Please provide all required field' });
+      }else{
+        Quotation.update(req.params.id, new Quotation(req.body), function(err, quotation) {
+          res.json({ error:false, message: 'Quotation successfully updated' });
+    });
+  }
 };
 exports.delete = function(req, res) {
   Quotation.delete( req.params.id, function(err, quotation) {
